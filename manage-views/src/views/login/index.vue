b4f406b2bce23dd4c85dc38a7a1e3fe8aa21e82f
ykn <sxczykn@163.com>
2020-02-18 22:20:55 +0800

    edit manage-views

--- manage-views/src/views/login/index.vue
+++ manage-views/src/views/login/index.vue
@@ -43,9 +43,7 @@
           @keyup.enter.native="handleLogin"
         />
         <span class="show-pwd" @click="showPwd">
-          <svg-icon
-            :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'"
-          />
+          <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" />
         </span>
       </el-form-item>
 
@@ -54,13 +52,11 @@
         type="primary"
         style="width:100%;margin-bottom:30px;"
         @click.native.prevent="handleLogin"
-      >
-        Login
-      </el-button>
+      >Login</el-button>
 
       <div class="tips">
-        <span style="margin-right:20px;">username: admin</span>
-        <span> password: any</span>
+        <span style="margin-right:20px;">username: any</span>
+        <span>password: any</span>
       </div>
     </el-form>
   </div>
