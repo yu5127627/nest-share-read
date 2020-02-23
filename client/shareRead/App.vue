d4d97973d247ee7755cc7e03ab7d6ba2bfa75d9c
ykn <sxczykn@163.com>
2020-02-23 23:41:58 +0800

    manage-view add app setting

--- client/shareRead/App.vue
+++ client/shareRead/App.vue
@@ -5,8 +5,8 @@
 	export default {
 		globalData: {
 			token: null,
-			api:'http://localhost:8001/'
-			// api:'http://read.sxczykn.top/'
+			// api:'http://localhost:8001/'
+			api:'http://read.sxczykn.top/'
 		},
 		onLaunch() {
 			getApp().globalData.token = uni.getStorageSync('token')
